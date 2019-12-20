71cbdfdab89eeab1ca0f9add597cb512b7b10023
Ameya Shringi <ameya.shringi@kitware.com>
2019-12-20 16:29:56 -0500

    Add more attributed to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from skbuild import setup
 import os.path as osp
 
+with open('VERSION', 'r') as f:
+    version = f.read().strip()
+
+with open('README.md', 'r') as f:
+    long_description = f.read().strip()
 
 setup( name='external_arrow',
-       version='0.0.1',
+       version=version,
+       long_description=long_description,
        packages=[ 'external_arrow',
                   'external_arrow.arrow',
                 ],
+       platforms=[
+                    'linux',
+                    'Unix'
+                 ],
        setup_requires=[
                         'setuptools',
+                        'cmake',
+                        'scikit-build'
                       ],
        install_requires=[
-                            'kwiver',
+                            'kwiver==1.4.2',
                         ],
        cmake_args=[
                     '-DCMAKE_BUILD_TYPE=Release',
@@ -32,5 +44,13 @@ setup( name='external_arrow',
        classifiers=[
                         "Development Status :: 3 - Alpha",
                         "Topics :: Test",
-                   ]
+                        "Intended Audience :: Developers",
+                        "Programming Language :: Python :: 3.5",
+                        "Programming Language :: Python :: 3.6",
+                        "Programming Language :: Python :: 3.7",
+                        "Programming Language :: Python :: 3.8",
+                        "Operating System :: Unix",
+                        "License :: OSI Approved :: BSD License"
+                   ],
+       python_requires=">=3.5",
     )
